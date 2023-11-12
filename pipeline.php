6fe317444f0d431869e792f8bc66b334128c9b07
Amp-Apirak <apirak.ba@gmail.com>
2023-11-12 13:50:01 +0700

    A

--- pipeline.php
+++ pipeline.php
@@ -786,7 +786,7 @@
                 </div>
                 <div class="modal-footer justify-content-between">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" name="submit" value="submit" class="btn btn-success">Save</button>
+                    
                 </div>
                 </form>
             </div>

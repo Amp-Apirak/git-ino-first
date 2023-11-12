1e5ceca3e4b7fdae1bd0699ad1d83eb58e716660
Amp-Apirak <apirak.ba@gmail.com>
2023-11-12 13:38:11 +0700

    A

--- pipeline.php
+++ pipeline.php
@@ -766,7 +766,7 @@
 
                                 <div class="col col-6">
                                     <div class="form-group">
-                                        <label for="fullname">Add Business to Goverment (B to G)</label>
+                                        <label for="fullname"><small>Add Business to Goverment (B to G)</small></label>
                                         <a href="pipeline_add.php" class="btn btn-success btn-sm float-right" > Business to Goverment <i class=""></i></a>
                                     </div>
                                 <!-- /.form-group -->
@@ -774,7 +774,7 @@
 
                                 <div class="col col-6">
                                     <div class="form-group">
-                                        <label for="fullname">Add Business to Business (B to B)</label>
+                                        <label for="fullname"><small>Add Business to Business (B to B)</small></label>
                                         <a href="pipeline_add1.php" class="btn btn-success btn-sm float-right" > Business to Business <i class=""></i></a>
                                     </div>
                                 <!-- /.form-group -->

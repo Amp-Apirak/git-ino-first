a4aad9973a82ffe686f03a73b318fdb42f451783
Amp-Apirak <apirak.ba@gmail.com>
2024-01-10 14:28:20 +0700

    :hammer: edit style electment

--- login.php
+++ login.php
@@ -35,17 +35,16 @@
     <!--Login Wrapper-->
     <div class="container-fluid login-wrapper">
         <div class="login-box">
-            <h1 class="text-center mb-5"><i class="fas fa-handshake text-primary"></i> Sale Service Management</h1>
+            <h1 class="text-center mb-5"><i class="fas fa-handshake text-primary"></i> Point IT Sales Management</h1>
             <div class="row">
                 <div class="col-md-6 col-sm-6 col-12 login-box-info">
                     <h3 class="mb-4">Sign up</h3>
-                    <p class="mb-4">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
+                    <p class="mb-4"></p>
                     <p class="text-center"><a href="regis.php" class="btn btn-light">Register here</a></p>
                 </div>
                 <div class="col-md-6 col-sm-6 col-12 login-box-form p-4">
                     <h3 class="mb-2">Login</h3>
-                    <small class="text-muted bc-description">Sign in with your credentials</small>
+                    <small class="text-muted bc-description"></small>
                     <form action="checklog.php" method="post" class="mt-2">
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
